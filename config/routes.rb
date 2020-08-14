9b7e8df12cd91ed3724f375efeb40a6c058e3260
Kimberly Dohmen <kimberly.dohmen@web.de>
2020-08-14 16:01:37 +0200

    new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:new, :create, :show, :index] do
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   end
 
   resources :doses, only: [:destroy]
+  root to: 'cocktails#index'
 
 end
